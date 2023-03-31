889cc5ab7ebd36f1126003659e44e4ecd9efca0b
Abu Md. Nayeem Khan <66042919+AbuMdNayeemKhan@users.noreply.github.com>
2023-03-31 16:26:58 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+//==========global scope
+
+// var globalData = "I'm global data.";
+
+// console.log(globalData);
+
+// function localFun(){
+//     var localData = "I'm local data.";
+//     console.log(localData);
+// }
+// localFun();
+
+// {
+//     let blockData = "I'm Block scope";
+//     console.log(blockData);
+// }
+// console.log(blockData);
+
+//==========Function Expresion
+
+// function callMe(){
+//     console.log("I'm the normal function");
+// }
+// callMe();
+
+var newCallMe = function callMe(){
+    console.log("I'm the new cll me function");
+}
+newCallMe();
+
+
+
